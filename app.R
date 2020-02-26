e22a7ab45773a0935c49d296d5dc07961314bcf1
jupyter <bdauria@est-econ.uc3m.es>
2020-02-26 10:35:23 +0100

    gapminder set table and plot

--- app.R
+++ app.R
@@ -1,8 +1,53 @@
 library(shiny)
+library(tidyverse)
+library(magrittr)
+library(gapminder)
 
-ui <- bootstrapPage()
+gapminder %<>% mutate_at(c("year", "country"), as.factor)
 
-server <- function(input, output) {}
+gapminder_years <- unique(gapminder$year)
+gapminder_countries <- unique(gapminder$country) %>% str_sort()
+
+headerRow <- div(
+                selectInput("selYear", 
+                         label="Select the Year", 
+                         multiple = TRUE,
+                         choices=gapminder_years),
+                selectInput("selCountry", 
+                            label="Select the Coutry", 
+                            multiple = TRUE,
+                            choices=gapminder_countries)
+)
+
+dataPanel <- tabPanel("Data",
+                      tableOutput("dataTable")
+             )
+
+plotPanel <- tabPanel("Plot",
+                      plotOutput("plotData"))
+
+ui <- navbarPage("Shiny App",
+                 dataPanel,
+                 plotPanel,
+                 header = headerRow
+)
+
+server <- function(input, output) {
+  
+  gapminder_filtered <- reactive(gapminder %>% filter(year %in% input$selYear, country %in% input$selCountry))
+  
+  output$dataTable <- renderTable({
+    req(input$selYear)
+    gapminder_filtered()
+    })
+  output$plotData <- renderPlot({
+    req(input$selCountry)
+    req(input$selYear)
+    gapminder_filtered() %>%
+      ggplot(aes(x=country, y=pop, fill=year)) +
+      geom_bar(stat="identity", position=position_dodge())
+    })
+}
 
 # Run the application 
 shinyApp(ui = ui, server = server)
